cb1a07d5754512635a82afcb908dd1a9f2a1b3d1
Oscar Torres <os.torresp@duocuc.cl>
2026-09-21 01:19:21 -0300

    Polimorfismo con interfaces

--- src/Main.java
+++ src/Main.java
@@ -16,6 +16,15 @@ public class Main {
         dron.atenderEmergencia();
         tecnica.atenderEmergencia();
         tecnica.reparar();
-
+        EquipoEmergencia equipo1 = new BrigadaMedica("Brigada Medica");
+        EquipoEmergencia equipo2 = new BrigadaTecnica("Brigada Tecnica");
+        EquipoEmergencia equipo3 = new DronRescate("Dron de Rescate");
+        equipo1.atenderEmergencia();
+        equipo2.atenderEmergencia();
+        equipo3.atenderEmergencia();
+        Volador volador = new DronRescate("Dron Aereo");
+        volador.volar();
+        Reparador reparador = new BrigadaTecnica("Equipo Tecnico");
+        reparador.reparar();
     }
 }
